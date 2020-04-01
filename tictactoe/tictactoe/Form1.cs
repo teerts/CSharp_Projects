ae35e3ae2cba39649820dc6324c7f65d2b0d78c7
Gerry Grewall <gobind99@gmail.com>
2020-04-01 11:03:03 -0700

    added stability for AIMove() to when buttons list reaches 0; refactored Check function

--- tictactoe/tictactoe/Form1.cs
+++ tictactoe/tictactoe/Form1.cs
@@ -31,21 +31,24 @@ namespace tictactoe
             button.Enabled = false; // disable the button that was clicked
             button.BackColor = System.Drawing.Color.Cyan; // set player colour
             buttons.Remove(button); // remove the button from the list, so AI cannot use it
-            Check(); // check if player has won the game
+            Check(currentPlayer.ToString()); // check if player has won the game
             AImoves.Start(); // start the timer for AI's move
         }
 
         private void AImove(object sender, EventArgs e)
        {
-           if (buttons.Count > 0)
+           if (buttons.Count >= 0)
            {
                int index = rand.Next(buttons.Count); // RNG: next button selected
-               buttons[index].Enabled = false; // assign RNG number to button
+               if (buttons.Count == 0)
+                {
+                    buttons[index].Enabled = false; // assign RNG number to button
+                }  
                currentPlayer = Player.O; // set AI to 'O' from player enum
                buttons[index].Text = currentPlayer.ToString(); // display 'O' on the button
                buttons[index].BackColor = System.Drawing.Color.DarkSalmon; // set color of bg
                buttons.RemoveAt(index); // remove button from selectable list
-               Check(); // check if winner yet
+               Check(currentPlayer.ToString()); // check if winner yet
                AImoves.Stop(); // stop the timer                
            }
        }
@@ -59,7 +62,7 @@ namespace tictactoe
        private void loadbuttons()
        {
            // load all buttons into a List
-           buttons = new List<Button> { button1, button2, button3, button4,                                  button5, button6, button7, button8 };
+           buttons = new List<Button> { button1, button2, button3, button4, button5, button6, button7, button8 };
        }
 
        private void resetGame()
@@ -80,29 +83,52 @@ namespace tictactoe
 
        // check if the player or AI won 
        // multiple large IF statements with winning possibilities
-       private void Check()
-       {
-            // definately cleaner for refactor; if any statements in fcn return true 
-           bool isFirstRowChecked = button1.Text == "X" && button2.Text == "X" && button3.Text == "X";
-
-           /* 1st 
-           if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X"
-               || button4.Text == "X" && button5.Text == "X" && button6.Text == "X"
-               || button7.Text == "X" && button8.Text == "X" && button9.Text == "X"
-               || button1.Text == "X" && button4.Text == "X" && button7.Text == "X"
-               || button2.Text == "X" && button5.Text == "X" && button8.Text == "X"
-               || button3.Text == "X" && button6.Text == "X" && button9.Text == "X"
-               || button1.Text == "X" && button5.Text == "X" && button9.Text == "X"
-               || button3.Text == "X" && button5.Text == "X" && button7.Text == "X") */
-           {
-               // if any statements above are true
-               AImoves.Stop();
-               MessageBox.Show("Player Wins");
-               playerWins++;
-               label1.Text = "Player Wins - " + playerWins;
-               resetGame();
-           }
-           else if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O"
+       private void Check(string playerIdentifier)
+       {         
+           bool isFirstRowChecked = button1.Text == playerIdentifier && button2.Text == playerIdentifier && button3.Text == playerIdentifier;
+           bool isMiddleRowChecked = button4.Text == playerIdentifier && button5.Text == playerIdentifier && button6.Text == playerIdentifier;
+           bool isLastRowChecked = button7.Text == playerIdentifier && button8.Text == playerIdentifier && button9.Text == playerIdentifier;
+           bool isFirstColumnChecked = button1.Text == playerIdentifier && button4.Text == playerIdentifier && button7.Text == playerIdentifier;
+           bool isMiddleColumnChecked = button2.Text == playerIdentifier && button5.Text == playerIdentifier && button8.Text == playerIdentifier;
+           bool isLastColumnChecked = button3.Text == playerIdentifier && button6.Text == playerIdentifier && button9.Text == playerIdentifier;
+           bool isDiagonalTopToBottomChecked = button1.Text == playerIdentifier && button5.Text == playerIdentifier && button9.Text == playerIdentifier;
+           bool isDiagonalBottomToTopChecked = button3.Text == playerIdentifier && button5.Text == playerIdentifier && button7.Text == playerIdentifier;
+            
+           if ((String.Equals(playerIdentifier, "X")) && (isFirstRowChecked || isMiddleRowChecked || isLastRowChecked || isFirstColumnChecked || isMiddleColumnChecked || isLastColumnChecked || isDiagonalTopToBottomChecked || isDiagonalBottomToTopChecked))
+            {
+                AImoves.Stop();
+                MessageBox.Show("Player Wins");
+                playerWins++;
+                label1.Text = "Player Wins - " + playerWins;
+                resetGame();
+            }
+           else if ((String.Equals(playerIdentifier, "O")) && (isFirstRowChecked || isMiddleRowChecked || isLastRowChecked || isFirstColumnChecked || isMiddleColumnChecked || isLastColumnChecked || isDiagonalTopToBottomChecked || isDiagonalBottomToTopChecked))
+            {
+                AImoves.Stop();
+                MessageBox.Show("Computer Wins");
+                computerWins++;
+                label2.Text = "AI Wins - " + computerWins;
+                resetGame();
+            }
+            // refactored code above; old commented code below
+            /* 1st 
+            if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X"
+                || button4.Text == "X" && button5.Text == "X" && button6.Text == "X"
+                || button7.Text == "X" && button8.Text == "X" && button9.Text == "X"
+                || button1.Text == "X" && button4.Text == "X" && button7.Text == "X"
+                || button2.Text == "X" && button5.Text == "X" && button8.Text == "X"
+                || button3.Text == "X" && button6.Text == "X" && button9.Text == "X"
+                || button1.Text == "X" && button5.Text == "X" && button9.Text == "X"
+                || button3.Text == "X" && button5.Text == "X" && button7.Text == "X") 
+            {
+                // if any statements above are true
+                AImoves.Stop();
+                MessageBox.Show("Player Wins");
+                playerWins++;
+                label1.Text = "Player Wins - " + playerWins;
+                resetGame();
+            } 
+            else if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O"
                || button4.Text == "O" && button5.Text == "O" && button6.Text == "O"
                || button7.Text == "O" && button8.Text == "O" && button9.Text == "O"
                || button1.Text == "O" && button4.Text == "O" && button7.Text == "O"
@@ -116,9 +142,7 @@ namespace tictactoe
                computerWins++;
                label2.Text = "AI Wins - " + computerWins;
                resetGame();
-            }
-       }
-
-
+            }*/
+        }
     }
 }
